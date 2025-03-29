91e16e330d06b1407bc97195bd02905e7b77e13d
Blooym <19539165+Blooym@users.noreply.github.com>
2025-03-29 17:41:09 +0100

    feat: allow wildcard octet-stream fallback

--- src/routes/uploads/upload.rs
+++ src/routes/uploads/upload.rs
@@ -8,10 +8,17 @@ use axum_extra::{
     TypedHeader,
     headers::{Authorization, authorization::Bearer},
 };
-use mime_guess::Mime;
+use mime_guess::{
+    Mime,
+    mime::{APPLICATION_OCTET_STREAM, STAR_STAR},
+};
 use serde::Serialize;
 use std::str::FromStr;
 use tracing::error;
+
+const FALLBACK_ENABLED_MIME: Mime = STAR_STAR;
+const FALLBACK_MIME_TYPE: Mime = APPLICATION_OCTET_STREAM;
+const FALLBACK_FILE_EXTENSION: &str = "unknown";
 
 #[derive(Serialize)]
 pub struct CreateUploadResponse {
@@ -48,15 +55,28 @@ pub async fn create_upload_handler(
     };
 
     // Infer mimetype by magic numbers and reject
-    // uploads that are not images or videos if enabled.
-    let Some(infer) = infer::get(&data) else {
+    let Ok((infer_str, infer_ext)) = infer::get(&data).map_or_else(
+        || {
+            // If wildcard mime is enabled, we can fallback to octet stream.
+            if state
+                .upload_allowed_mimetypes
+                .contains(&FALLBACK_ENABLED_MIME)
+            {
+                Ok((FALLBACK_MIME_TYPE.essence_str(), FALLBACK_FILE_EXTENSION))
+            } else {
+                Err(())
+            }
+        },
+        |f| Ok((f.mime_type(), f.extension())),
+    ) else {
         return Err((
             StatusCode::UNSUPPORTED_MEDIA_TYPE,
             "Your file was rejected because the MIME type could not be determined.",
         ));
     };
+
     if !mime::is_mime_allowed(
-        &Mime::from_str(infer.mime_type()).unwrap(),
+        &Mime::from_str(infer_str).unwrap(),
         &state.upload_allowed_mimetypes,
     ) {
         return Err((
@@ -72,12 +92,12 @@ pub async fn create_upload_handler(
             .unwrap()
             .get(..10)
             .unwrap(),
-        infer.extension()
+        infer_ext
     );
 
     match state.storage.save_file(&filename, &data) {
         Ok(decryption_key) => Ok(Json(CreateUploadResponse {
-            mimetype: infer.mime_type(),
+            mimetype: infer_ext,
             url: format!(
                 "{}://{}/upload/{}?key={}",
                 state.public_base_url.scheme(),
